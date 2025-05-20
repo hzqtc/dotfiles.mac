c36a0952c8d966a4d28b07ae6470921e511f9277
Johnny Huang <hzqtc1229@gmail.com>
2025-05-19 22:04:23 -0700

    Add a view command to view different files using different tools.

--- config.fish
+++ config.fish
@@ -7,10 +7,35 @@ set fish_greeting
 # Use vi key bindings
 set -g fish_key_bindings fish_vi_key_bindings
 
+function view
+  set file $argv[1]
+
+  if not test -f "$file"
+    echo "view: file not found: $file"
+    return 1
+  end
+
+  switch "$file"
+    case '*.json'
+      jless "$file"
+    case '*.md' '*.markdown'
+      glow -p "$file"
+    case '*.csv'
+      tw "$file"
+    case '*'
+      set filetype (file --brief "$file")
+      if string match -q '*text*' "$filetype"
+        bat "$file"
+      else
+        hexyl "$file"
+      end
+  end
+end
+
 starship init fish | source
 
 set -gx LEDGER_FILE /Users/hzqtc/Documents/hledger/hledger.journal
 set -gx PAGER moar
-set -gx EDITOR vim
+set -gx EDITOR nvim
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
 
--- zshrc
+++ zshrc
@@ -13,10 +13,39 @@ bindkey "^[[B" history-beginning-search-forward-end
 
 autoload zmv
 
+function view() {
+  local file="$1"
+
+  if [[ ! -f "$file" ]]; then
+    echo "view: file not found: $file"
+    return 1
+  fi
+
+  case "$file" in
+    *.json)
+      jless "$file"
+      ;;
+    *.md|*.markdown)
+      glow -p "$file"
+      ;;
+    *.csv)
+      tw "$file"
+      ;;
+    *)
+      local filetype=$(file --brief "$file")
+      if [[ "$filetype" == *"text"* ]]; then
+        bat "$file"
+      else
+        hexyl "$file"
+      fi
+      ;;
+  esac
+}
+
 eval "$(starship init zsh)"
 
 export LEDGER_FILE="/Users/hzqtc/Documents/hledger/hledger.journal"
 export PAGER=moar
-export EDITOR=vim
+export EDITOR=nvim
 export HOMEBREW_NO_AUTO_UPDATE=1
 
